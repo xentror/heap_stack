ffc3ea2f0daa42d996f3f1f024936990e6aa5832
Thibault vivies <thibault.vivies@epita.fr>
2020-03-11 09:54:03 +0100

    heap_stack/list: clear and add tests for the list implementation.

--- generic_list/glist-test.c
+++ generic_list/glist-test.c
@@ -43,9 +43,8 @@ void print_integ_list(integ_list_t *list)
     printf("%d }", list->values[i]);
 }
 
-int main(void)
+void push_and_pop()
 {
-    integ_list_t *new_node = NULL;
     integ_list_t *l = integ_list_create();
     l->values[0] = -1;
     l->values[1] = -1;
@@ -54,7 +53,7 @@ int main(void)
         integ_list_t *tmp_node = integ_list_create();
 
         if (!tmp_node)
-            return 1;
+            return;
 
         tmp_node->values[0] = i;
         l = integ_list_push(l, tmp_node);
@@ -64,22 +63,74 @@ int main(void)
 
     integ_list_print(l, print_integ_list);
 
-    for (int i = 0; i < 3; i++) {
+    for (int i = 0; i < 11; i++) {
         l = integ_list_pop(l);
         integ_list_print(l, print_integ_list);
     }
 
-    assert (integ_list_get_size(l) == 8);
+    assert (l == NULL);
+    assert (integ_list_get_size(l) == 0);
+}
 
-    new_node = integ_list_create();
-    new_node->values[0] = 678;
-    l = integ_list_push(l, new_node);
+void insert_and_delete_front()
+{
+    integ_list_t *new_node = NULL;
+    integ_list_t *l = integ_list_create();
+    l->values[0] = -1;
+    l->values[1] = -1;
+
+    for (int i = 0; i < 10; i++) {
+        new_node = integ_list_create();
+        new_node->values[0] = i;
+        l = integ_list_insert_at(l, new_node, 0);
+    }
+    integ_list_print(l, print_integ_list);
+
+    for (int i = 0; i < 10; i++) {
+        l = integ_list_delete_at(l, 0);
+        integ_list_print(l, print_integ_list);
+    }
     integ_list_print(l, print_integ_list);
 
     integ_list_erase(&l);
-    integ_list_print(l, print_integ_list);
-
     assert (l == NULL);
     assert (integ_list_get_size(l) == 0);
+}
+
+void insert_delete_middle()
+{
+    integ_list_t *new_node = NULL;
+    integ_list_t *l = integ_list_create();
+    l->values[0] = -1;
+    l->values[1] = -1;
+
+    new_node = integ_list_create();
+    new_node->values[0] = 0;
+    l = integ_list_push(l, new_node);
+
+    new_node = integ_list_create();
+    new_node->values[0] = 2;
+    l = integ_list_push(l, new_node);
+
+    new_node = integ_list_create();
+    new_node->values[0] = 1;
+    l = integ_list_insert_at(l, new_node, 2);
+
+    integ_list_print(l, print_integ_list);
+
+    l = integ_list_delete_at(l, 2);
+    integ_list_print(l, print_integ_list);
+
+    integ_list_erase(&l);
+    assert (l == NULL);
+    assert (integ_list_get_size(l) == 0);
+}
+
+int main(void)
+{
+    push_and_pop();
+    insert_and_delete_front();
+    insert_delete_middle();
+
     return 0;
 }
